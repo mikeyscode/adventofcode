c1155340604debf8e9a032fea97d0631150b98c5
Mikey Leah <michael.leah@purplewifi.com>
2018-12-03 11:51:12 +0000

    Added repeated steps to handle repeated frequency duplication problem

--- 2018/day_01/main.go
+++ 2018/day_01/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
+
+var knownFrequencies map[int]interface{}
 
 func main() {
 	file, err := os.Open("./input.txt")
@@ -14,14 +17,32 @@ func main() {
 	}
 	defer file.Close()
 
+	i := 0
+	total := 0
+	knownFrequencies = make(map[int]interface{})
+	for i < 1000 {
+		file.Seek(0, 0)
+		total = scanFile(file, total)
+		i++
+	}
+
+}
+
+func scanFile(file io.Reader, total int) int {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-
-	total := 0
 	for scanner.Scan() {
 		i, _ := strconv.Atoi(scanner.Text())
 		total += i
+
+		if _, ok := knownFrequencies[total]; ok {
+			fmt.Println(total)
+			os.Exit(1)
+		}
+
+		knownFrequencies[total] = total
+
 	}
 
-	fmt.Println(total)
+	return total
 }
